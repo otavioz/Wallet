bc05d313d4001539bb6e1043389314720ff7f1ee
Otavio <otaviorodriguesv@hotmail.com>
2023-12-17 11:27:35 -0300

    change the logging level

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import consts as CONS
 
 # Enable logging
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO,filename=f'log.log'
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.WARNING,filename=f'log.log'
 )
 logger = logging.getLogger(__name__)
 
